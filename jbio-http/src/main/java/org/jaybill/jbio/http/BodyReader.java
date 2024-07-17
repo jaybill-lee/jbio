4e2586dffe5e622345c6f4fe8a04410504b7ddaf
jaybill <m18813296645@163.com>
2024-07-17 21:45:33 +0800

    fix read body bug

--- jbio-http/src/main/java/org/jaybill/jbio/http/BodyReader.java
+++ jbio-http/src/main/java/org/jaybill/jbio/http/BodyReader.java
@@ -22,14 +22,19 @@ public class BodyReader {
     Deque<ByteBuffer> readFixLengthBody(ByteBuffer src) {
         var remaining = fixLengthBodyBuf.remaining();
         var srcRemaining = src.remaining();
-        fixLengthBodyBuf.put(src.duplicate()
+        var dupSrc = src.duplicate()
                 .position(src.position())
-                .limit(srcRemaining > remaining ? src.position() + remaining : src.limit()));
+                .limit(srcRemaining > remaining ? src.position() + remaining : src.limit());
+        fixLengthBodyBuf.put(dupSrc);
         if (fixLengthBodyBuf.remaining() != 0) {
             return null;
         }
 
+        // update src pos
+        src.position(dupSrc.position());
+
         // read completed
+        fixLengthBodyBuf.flip();
         buffers.add(fixLengthBodyBuf);
         return buffers;
     }
